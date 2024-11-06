d37b2be108c502894a802653e60e85c936d820de
AngeloCrisci <angelocrisci1998@gmail.com>
2024-11-06 18:17:03 +0100

    Modified Models with hasmany and belongs to

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
@@ -14,5 +15,9 @@ class Project extends Model
         "date",
         "description",
         "link",
+        "types_id",
     ];
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
